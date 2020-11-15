a328696279c0c29d10d00cbe37b885b10034b346
Musthafa Faishal <musthafafaishal@gmail.com>
2020-11-15 11:20:52 +0700

    Undo boolean.hpp

--- list-linear/listl/boolean.hpp
+++ list-linear/listl/boolean.hpp
@@ -2,10 +2,10 @@
 #define boolean_H
 
 /***********************************/
-/* Program      : boolean.hpp */
-/* Description  : header file modul boolean */
-/* ID/Name      : 24060118120042/Musthafa Kamal Faishal */
-/* Date         : 7/11/2020 */
+/* Program    : boolean.hpp */
+/* Deskripsi  : header file modul boolean */
+/* NIM/Nama   : 24060118120042/Musthafa Kamal Faishal */
+/* Tanggal    : 7/11/2020 */
 /***********************************/
 
 #define true 1
